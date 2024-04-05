afb0a720101b548091a96e59af7a34516b9914a4
DrxcoDev <drxcovalorant@gmail.com>
2024-04-05 15:59:52 +0200

    first commit

---
+++ MyModules/loadingdropbox/playground/app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// alternatively, you can also use it here
+const { $hello } = useNuxtApp()
+</script>
+
+<template>
+  <div>
+    {{ $hello('world') }}
+  </div>
+</template>
