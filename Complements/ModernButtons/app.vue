c8a337161969601fc5c659f253e10775e40fa4ce
DrxcoDev <drxcovalorant@gmail.com>
2024-04-06 21:25:56 +0200

    New update

--- Complements/ModernButtons/app.vue
+++ Complements/ModernButtons/app.vue
@@ -5,25 +5,48 @@
             <div class="d-inline">
                 <div style="padding: 100px;">
                     <div class="mt-4">
-                        <a class="p-3 text-light border border-white rounded" style="width: 60px;height: 20px;text-decoration: none;">Border White</a>
+                        <a 
+                        class="p-3 text-light border border-white rounded"
+                        style="width: 60px;height: 20px;text-decoration: none;"
+                        :class="{ 'btn-primary': !isHovered, 'btn-secondary': isHovered }"
+                        @mouseover="isHovered = true"
+                        @mouseout="isHovered = false">Border White</a>
                     </div>
                     <div class="mt-6" style="margin-top: 50px;">
-                        <a class="p-3 text-primary border border-primary rounded" style="width: 60px;height: 20px;text-decoration: none;">Border Blue</a>
+                        <a class="p-3 text-primary border border-primary rounded" style="width: 60px;height: 20px;text-decoration: none;"
+                        :class="{ 'btn-primary': !isHovered, 'btn-secondary': isHovered }"
+                        @mouseover="isHovered = true"
+                        @mouseout="isHovered = false">Border Blue</a>
                     </div>
                     <div class="mt-6" style="margin-top: 50px;">
-                        <a class="p-3 text-success border border-success rounded" style="width: 60px;height: 20px;text-decoration: none;">Border Green</a>
+                        <a class="p-3 text-success border border-success rounded" style="width: 60px;height: 20px;text-decoration: none;"
+                        :class="{ 'btn-primary': !isHovered, 'btn-secondary': isHovered }"
+                        @mouseover="isHovered = true"
+                        @mouseout="isHovered = false">Border Green</a>
                     </div>
                     <div class="mt-6" style="margin-top: 50px;">
-                        <a class="p-3 text-danger border border-danger rounded" style="width: 60px;height: 20px;text-decoration: none;">Border Red</a>
+                        <a class="p-3 text-danger border border-danger rounded" style="width: 60px;height: 20px;text-decoration: none;"
+                        :class="{ 'btn-primary': !isHovered, 'btn-secondary': isHovered }"
+                        @mouseover="isHovered = true"
+                        @mouseout="isHovered = false">Border Red</a>
                     </div>
                     <div class="mt-6" style="margin-top: 50px;">
-                        <a class="p-3 text-warning border border-warning rounded" style="width: 60px;height: 20px;text-decoration: none;">Border White</a>
+                        <a class="p-3 text-warning border border-warning rounded" style="width: 60px;height: 20px;text-decoration: none;"
+                        :class="{ 'btn-primary': !isHovered, 'btn-secondary': isHovered }"
+                        @mouseover="isHovered = true"
+                        @mouseout="isHovered = false">Border White</a>
                     </div>
                     <div class="mt-6" style="margin-top: 50px;">
-                        <a class="p-3 text-success-emphasis border border-success-emphasis rounded" style="width: 60px;height: 20px;text-decoration: none;">Border Light green</a>
+                        <a class="p-3 text-success-emphasis border border-success-emphasis rounded" style="width: 60px;height: 20px;text-decoration: none;"
+                        :class="{ 'btn-primary': !isHovered, 'btn-secondary': isHovered }"
+                        @mouseover="isHovered = true"
+                        @mouseout="isHovered = false">Border Light green</a>
                     </div>
                     <div class="mt-6" style="margin-top: 50px;">
-                        <a class="p-3 text-info border border-info rounded" style="width: 60px;height: 20px;text-decoration: none;border: cyan 2px solid;">Border cyan</a>
+                        <a class="p-3 text-info border border-info rounded" style="width: 60px;height: 20px;text-decoration: none;border: cyan 2px solid;"
+                        :class="{ 'btn-primary': !isHovered, 'btn-secondary': isHovered }"
+                        @mouseover="isHovered = true"
+                        @mouseout="isHovered = false">Border cyan</a>
                     </div>
                     
                 </div>
@@ -32,25 +55,46 @@
             <div class="d-inline">
                 <div style="padding: 100px;">
                     <div class="mt-4">
-                        <a class="p-3 text-black border bg-light border-white rounded" style="width: 60px;height: 20px;text-decoration: none;">Classic</a>
+                        <a class="p-3 text-black border bg-light border-white rounded" style="width: 60px;height: 20px;text-decoration: none;"
+                        :class="{ 'btn-primary': !isHovered, 'btn-secondary': isHovered }"
+                        @mouseover="isHovered = true"
+                        @mouseout="isHovered = false">Classic</a>
                     </div>
                     <div class="mt-6" style="margin-top: 50px;">
-                        <a class="p-3 text-light border bg-primary border-primary rounded" style="width: 60px;height: 20px;text-decoration: none;">Border Blue</a>
+                        <a class="p-3 text-light border bg-primary border-primary rounded" style="width: 60px;height: 20px;text-decoration: none;"
+                        :class="{ 'btn-primary': !isHovered, 'btn-secondary': isHovered }"
+                        @mouseover="isHovered = true"
+                        @mouseout="isHovered = false">Border Blue</a>
                     </div>
                     <div class="mt-6" style="margin-top: 50px;">
-                        <a class="p-3 text-light bg-success border border-success rounded" style="width: 60px;height: 20px;text-decoration: none;">Border Green</a>
+                        <a class="p-3 text-light bg-success border border-success rounded" style="width: 60px;height: 20px;text-decoration: none;"
+                        :class="{ 'btn-primary': !isHovered, 'btn-secondary': isHovered }"
+                        @mouseover="isHovered = true"
+                        @mouseout="isHovered = false">Border Green</a>
                     </div>
                     <div class="mt-6" style="margin-top: 50px;">
-                        <a class="p-3 text-light bg-danger border border-danger rounded" style="width: 60px;height: 20px;text-decoration: none;">Border Red</a>
+                        <a class="p-3 text-light bg-danger border border-danger rounded" style="width: 60px;height: 20px;text-decoration: none;"
+                        :class="{ 'btn-primary': !isHovered, 'btn-secondary': isHovered }"
+                        @mouseover="isHovered = true"
+                        @mouseout="isHovered = false">Border Red</a>
                     </div>
                     <div class="mt-6" style="margin-top: 50px;">
-                        <a class="p-3 text-light bg-warning border border-warning rounded" style="width: 60px;height: 20px;text-decoration: none;">Border White</a>
+                        <a class="p-3 text-light bg-warning border border-warning rounded" style="width: 60px;height: 20px;text-decoration: none;"
+                        :class="{ 'btn-primary': !isHovered, 'btn-secondary': isHovered }"
+                        @mouseover="isHovered = true"
+                        @mouseout="isHovered = false">Border White</a>
                     </div>
                     <div class="mt-6" style="margin-top: 50px;">
-                        <a class="p-3 text-black bg-success-subtle border border-success-emphasis rounded" style="width: 60px;height: 20px;text-decoration: none;">Border Light green</a>
+                        <a class="p-3 text-black bg-success-subtle border border-success-emphasis rounded" style="width: 60px;height: 20px;text-decoration: none;"
+                        :class="{ 'btn-primary': !isHovered, 'btn-secondary': isHovered }"
+                        @mouseover="isHovered = true"
+                        @mouseout="isHovered = false">Border Light green</a>
                     </div>
                     <div class="mt-6" style="margin-top: 50px;">
-                        <a class="p-3 text-light bg-info border-info border rounded" style="width: 60px;height: 20px;text-decoration: none;border: cyan 2px solid;">Border cyan</a>
+                        <a class="p-3 text-light bg-info border-info border rounded" style="width: 60px;height: 20px;text-decoration: none;border: cyan 2px solid;"
+                        :class="{ 'btn-primary': !isHovered, 'btn-secondary': isHovered }"
+                        @mouseover="isHovered = true"
+                        @mouseout="isHovered = false">Border cyan</a>
                     </div>
                     
                 </div>
@@ -86,4 +130,29 @@
         </div>
         
     </body>
+
+    
 </template>
+
+
+<script>
+        export default {
+            data() {
+                return {
+                isHovered: false,
+                };
+            }
+        };
+</script>
+<style>
+        .btn-primary {
+            margin-top: 0px;
+            transition-duration: 1.2s;
+            position: inherit;
+            }
+
+        .btn-secondary {
+            margin-left: 20px;
+            transition-duration: 1.2s;
+            }
+    </style>
---
+++ MyModules/elementplus/app.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!-- Botón de Element+ -->
+    <el-button @click="showDialog">Mostrar Diálogo</el-button>
+
+    <!-- Cuadro de diálogo de Element+ -->
+    <el-dialog
+      :visible.sync="dialogVisible"
+      title="Diálogo de Ejemplo"
+      width="30%"
+      :close-on-click-modal="false"
+    >
+      <p>¡Este es un cuadro de diálogo de Element+!</p>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancelar</el-button>
+        <el-button type="primary" @click="dialogVisible = false">Aceptar</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { ElButton, ElDialog } from "element-plus";
+
+const dialogVisible = ref(false);
+
+const showDialog = () => {
+  dialogVisible.value = true;
+};
+</script>
+
+<style>
+/* Estilos de Element+ aquí */
+</style>
