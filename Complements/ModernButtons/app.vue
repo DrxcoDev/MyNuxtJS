3e53ed53e6990573501242fc34917af3c50d7255
DrxcoDev <drxcovalorant@gmail.com>
2024-04-06 11:32:54 +0200

    New complement

---
+++ Complements/ModernButtons/app.vue
@@ -0,0 +1,89 @@
+<template>
+    <body class="bg-black text-light">
+        <div class="d-flex" style="margin-left: 100px;">
+
+            <div class="d-inline">
+                <div style="padding: 100px;">
+                    <div class="mt-4">
+                        <a class="p-3 text-light border border-white rounded" style="width: 60px;height: 20px;text-decoration: none;">Border White</a>
+                    </div>
+                    <div class="mt-6" style="margin-top: 50px;">
+                        <a class="p-3 text-primary border border-primary rounded" style="width: 60px;height: 20px;text-decoration: none;">Border Blue</a>
+                    </div>
+                    <div class="mt-6" style="margin-top: 50px;">
+                        <a class="p-3 text-success border border-success rounded" style="width: 60px;height: 20px;text-decoration: none;">Border Green</a>
+                    </div>
+                    <div class="mt-6" style="margin-top: 50px;">
+                        <a class="p-3 text-danger border border-danger rounded" style="width: 60px;height: 20px;text-decoration: none;">Border Red</a>
+                    </div>
+                    <div class="mt-6" style="margin-top: 50px;">
+                        <a class="p-3 text-warning border border-warning rounded" style="width: 60px;height: 20px;text-decoration: none;">Border White</a>
+                    </div>
+                    <div class="mt-6" style="margin-top: 50px;">
+                        <a class="p-3 text-success-emphasis border border-success-emphasis rounded" style="width: 60px;height: 20px;text-decoration: none;">Border Light green</a>
+                    </div>
+                    <div class="mt-6" style="margin-top: 50px;">
+                        <a class="p-3 text-info border border-info rounded" style="width: 60px;height: 20px;text-decoration: none;border: cyan 2px solid;">Border cyan</a>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div class="d-inline">
+                <div style="padding: 100px;">
+                    <div class="mt-4">
+                        <a class="p-3 text-black border bg-light border-white rounded" style="width: 60px;height: 20px;text-decoration: none;">Classic</a>
+                    </div>
+                    <div class="mt-6" style="margin-top: 50px;">
+                        <a class="p-3 text-light border bg-primary border-primary rounded" style="width: 60px;height: 20px;text-decoration: none;">Border Blue</a>
+                    </div>
+                    <div class="mt-6" style="margin-top: 50px;">
+                        <a class="p-3 text-light bg-success border border-success rounded" style="width: 60px;height: 20px;text-decoration: none;">Border Green</a>
+                    </div>
+                    <div class="mt-6" style="margin-top: 50px;">
+                        <a class="p-3 text-light bg-danger border border-danger rounded" style="width: 60px;height: 20px;text-decoration: none;">Border Red</a>
+                    </div>
+                    <div class="mt-6" style="margin-top: 50px;">
+                        <a class="p-3 text-light bg-warning border border-warning rounded" style="width: 60px;height: 20px;text-decoration: none;">Border White</a>
+                    </div>
+                    <div class="mt-6" style="margin-top: 50px;">
+                        <a class="p-3 text-black bg-success-subtle border border-success-emphasis rounded" style="width: 60px;height: 20px;text-decoration: none;">Border Light green</a>
+                    </div>
+                    <div class="mt-6" style="margin-top: 50px;">
+                        <a class="p-3 text-light bg-info border-info border rounded" style="width: 60px;height: 20px;text-decoration: none;border: cyan 2px solid;">Border cyan</a>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div class="d-inline">
+                <div style="padding: 100px;">
+                    <div class="mt-4">
+                        <a class="p-3 text-black border bg-light border-white " style="width: 60px;height: 20px;text-decoration: none;">Classic</a>
+                    </div>
+                    <div class="mt-6" style="margin-top: 50px;">
+                        <a class="p-3 text-light border bg-primary border-primary " style="width: 60px;height: 20px;text-decoration: none;">Border Blue</a>
+                    </div>
+                    <div class="mt-6" style="margin-top: 50px;">
+                        <a class="p-3 text-light bg-success border border-success " style="width: 60px;height: 20px;text-decoration: none;">Border Green</a>
+                    </div>
+                    <div class="mt-6" style="margin-top: 50px;">
+                        <a class="p-3 text-light bg-danger border border-danger " style="width: 60px;height: 20px;text-decoration: none;">Border Red</a>
+                    </div>
+                    <div class="mt-6" style="margin-top: 50px;">
+                        <a class="p-3 text-light bg-warning border border-warning " style="width: 60px;height: 20px;text-decoration: none;">Border White</a>
+                    </div>
+                    <div class="mt-6" style="margin-top: 50px;">
+                        <a class="p-3 text-black bg-success-subtle border border-success-emphasis " style="width: 60px;height: 20px;text-decoration: none;">Border Light green</a>
+                    </div>
+                    <div class="mt-6" style="margin-top: 50px;">
+                        <a class="p-3 text-light bg-info border-info border" style="width: 60px;height: 20px;text-decoration: none;border: cyan 2px solid;">Border cyan</a>
+                    </div>
+                    
+                </div>
+            </div>
+
+        </div>
+        
+    </body>
+</template>
--- Complements/loadingdropbox/README.md
+++ Complements/loadingdropbox/README.md
@@ -1,4 +1,4 @@
-# Nuxt 3 Minimal Starter
+# Modern Loading Box
 
 Look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
 
